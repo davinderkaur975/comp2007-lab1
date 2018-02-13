7b085ef7e325b1c98f660f22c1851df65d5ce415
Davinder Kaur <davinderkaur975@gmail.com>
2018-02-13 17:36:37 -0500

    lesson 1

---
+++ Lab1/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Details(String product)
+        {
+            ViewBag.message = "You selected product:   " +product;
+            return View();
+        }
+    }
+}
